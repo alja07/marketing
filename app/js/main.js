1e75354722eab7f1a2db52ecbb6893d374f864f2
alja07 <alyakidinova@yandex.ru>
2021-09-01 18:27:46 +0700

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,36 @@
+$(function() {
+  
+  $('.menu a, .to-top').on('click', function (e) {
+    e.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+  
+  $('.slider-blog__inner').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg"></button>',
+    responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        arrows: false
+      }
+    },
+  ]
+});
+    
+
+  $('.menu__btn, .menu a').on('click', function(){
+    $('.header__top-inner').toggleClass('header__top-inner--active');
+
+  });
+  
+
+
+
+  var mixer = mixitup('.portfolio__content');
+
+});
